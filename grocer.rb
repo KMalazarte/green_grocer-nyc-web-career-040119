df03dacf99abf7140675975a3ab50734638371c0
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-04-01 07:09:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,34 @@
+require "pry"
+
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |item|
+    item.each do |name, data|
+      if new_hash[name]
+      new_hash[name][:count] += 1 
+      elsif
+      new_hash[name]= data
+      new_hash[name][:count] = 1
+      end
+    end
+  end
+  
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon|
+    name = coupon[:item]
+  end
 end
 
 def apply_clearance(cart)
-  # code here
+  cart.each do |item|
+    if item[1][:clearance]
+      new_price = item[1][:price]* 0.8
+      item[1][:price] = new_price.round(1)
+    end
+  end
 end
 
 def checkout(cart, coupons)
